69220ee364c07069f15e84f044182c50fb19a589
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-10-19 19:31:40 +0300

    Created PageRepository

--- src/main/java/com/myshop/cmsshoppingcart/controllers/AdminController.java
+++ src/main/java/com/myshop/cmsshoppingcart/controllers/AdminController.java
@@ -1,15 +1,26 @@
 package com.myshop.cmsshoppingcart.controllers;
 
+import com.myshop.cmsshoppingcart.models.PageRepository;
+import com.myshop.cmsshoppingcart.models.data.Page;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/pages")
 public class AdminController {
 
+    @Autowired
+    PageRepository pageRepository;
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        List<Page> pages = pageRepository.findAll();
+        model.addAttribute("pages", pages);
         return "admin/pages/index";
     }
 }
---
+++ src/main/java/com/myshop/cmsshoppingcart/models/PageRepository.java
@@ -0,0 +1,9 @@
+package com.myshop.cmsshoppingcart.models;
+
+import com.myshop.cmsshoppingcart.models.data.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PageRepository extends JpaRepository<Page, Integer> {
+}
