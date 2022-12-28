9678e0ffb1f1150bc9125d86b675cdf22e0b3464
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-12-28 16:55:00 +0200

    Created AdminCategoriesController and index page

---
+++ src/main/java/com/myshop/cmsshoppingcart/controllers/AdminCategoriesController.java
@@ -0,0 +1,31 @@
+package com.myshop.cmsshoppingcart.controllers;
+
+import com.myshop.cmsshoppingcart.models.CategoryRepository;
+import com.myshop.cmsshoppingcart.models.data.Category;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin/categories")
+public class AdminCategoriesController {
+
+    private CategoryRepository categoryRepository;
+
+    public AdminCategoriesController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping
+    public String index(Model model){
+        List<Category>  categories = categoryRepository.findAll();
+
+        model.addAttribute("categories", categories);
+
+        return "admin/categories/index";
+
+    }
+}
