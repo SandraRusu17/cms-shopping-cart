928a6733596924e894d4534bb60dc2e4e9152832
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-01-03 00:04:55 +0200

    Added POST add product endpoint. Added entity validation

--- src/main/java/com/myshop/cmsshoppingcart/controllers/AdminProductsController.java
+++ src/main/java/com/myshop/cmsshoppingcart/controllers/AdminProductsController.java
@@ -7,9 +7,18 @@ import com.myshop.cmsshoppingcart.models.data.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
@@ -21,7 +30,7 @@ public class AdminProductsController {
     private final CategoryRepository categoryRepository;
 
     @GetMapping
-    public String index(Model model){
+    public String index(Model model) {
 
         List<Product> products = productRepository.findAll();
 
@@ -36,6 +45,50 @@ public class AdminProductsController {
         List<Category> categories = categoryRepository.findAll();
         model.addAttribute("categories", categories);
         return "admin/products/add";
+    }
+
+    @PostMapping("/add")
+    public String add(@Valid Product product,
+                      BindingResult bindingResult,
+                      MultipartFile file,
+                      RedirectAttributes redirectAttributes,
+                      Model model) throws IOException {
+
+        if (bindingResult.hasErrors()) {
+            return "admin/categories/add";
+        }
+
+        boolean fileOK = false;
+        byte[] bytes = file.getBytes();
+        String filename = file.getOriginalFilename();
+        Path path = Paths.get("src/main/resources/static/media/" + filename);
+
+        if (filename.endsWith("jpg") || filename.endsWith("png")) {
+            fileOK = true;
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Product added");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        String slug = product.getName().toLowerCase().replace(" ", "-");
+
+        Product productExists = productRepository.findBySlug(slug);
+
+        if (!fileOK) {
+            redirectAttributes.addFlashAttribute("message", "Slug exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+        } else if (productExists != null) {
+            redirectAttributes.addFlashAttribute("message", "Product exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("product", product);
+        } else {
+            product.setSlug(slug);
+            product.setImage(filename);
+            productRepository.save(product);
+
+            Files.write(path, bytes);
+        }
+        return "redirect:/admin/products/add";
     }
 
 }
