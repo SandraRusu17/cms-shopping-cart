5d8f719a3c9e3dd7473a4ebf949e6571da70797e
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-01-10 21:52:20 +0200

    Added categories menu in pages

--- src/main/java/com/myshop/cmsshoppingcart/Common.java
+++ src/main/java/com/myshop/cmsshoppingcart/Common.java
@@ -1,8 +1,10 @@
 package com.myshop.cmsshoppingcart;
 
+import com.myshop.cmsshoppingcart.models.CategoryRepository;
 import com.myshop.cmsshoppingcart.models.PageRepository;
+import com.myshop.cmsshoppingcart.models.data.Category;
 import com.myshop.cmsshoppingcart.models.data.Page;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,16 +12,21 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import java.util.List;
 
 @ControllerAdvice
-@RequiredArgsConstructor
 public class Common {
 
+    @Autowired
     private PageRepository pageRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @ModelAttribute
     public void sharedData(Model model) {
         List<Page> pages = pageRepository.findAllByOrderBySortingAsc();
+        List<Category> categories = categoryRepository.findAll();
 
         model.addAttribute("cpages", pages);
+        model.addAttribute("ccategories", categories);
     }
 
 }
--- src/main/java/com/myshop/cmsshoppingcart/controllers/PageController.java
+++ src/main/java/com/myshop/cmsshoppingcart/controllers/PageController.java
@@ -2,23 +2,37 @@ package com.myshop.cmsshoppingcart.controllers;
 
 import com.myshop.cmsshoppingcart.models.PageRepository;
 import com.myshop.cmsshoppingcart.models.data.Page;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-@RequiredArgsConstructor
 public class PageController {
 
+    @Autowired
     private PageRepository pageRepository;
 
     @GetMapping
     public String home(Model model){
 
         Page page = pageRepository.findBySlug("home");
+        model.addAttribute("page", page);
+
+        return "page";
+    }
+
+    @GetMapping("/{slug}")
+    public String page(@PathVariable String slug,  Model model){
+
+        Page page = pageRepository.findBySlug(slug);
+
+        if (page == null){
+            return "redirect:/";
+        }
         model.addAttribute("page", page);
 
         return "page";
