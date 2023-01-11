1329d62df0b24a395837768d65934512a74c7199
SandraRusu <sandra.rusu.ext@extendaretail.com>
2023-01-11 21:13:09 +0200

    Implemented Add products to cart feature

---
+++ src/main/java/com/myshop/cmsshoppingcart/controllers/CartController.java
@@ -0,0 +1,61 @@
+package com.myshop.cmsshoppingcart.controllers;
+
+import com.myshop.cmsshoppingcart.models.ProductRepository;
+import com.myshop.cmsshoppingcart.models.data.Cart;
+import com.myshop.cmsshoppingcart.models.data.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/add/{id}")
+    public String add(@PathVariable int id, HttpSession session, Model model) {
+
+        Optional<Product> product = productRepository.findById(id);
+
+        if (session.getAttribute("cart") == null) {
+
+            HashMap<Integer, Cart> cart = new HashMap<>();
+            cart.put(id, new Cart(id, product.get().getName(), product.get().getPrice(), 1, product.get().getImage()));
+            session.setAttribute("cart", cart);
+        } else {
+            HashMap<Integer, Cart> cart = (HashMap<Integer, Cart>) session.getAttribute("cart");
+            if (cart.containsKey(id)) {
+                int qty = cart.get(id).getQuantity();
+                cart.put(id, new Cart(id, product.get().getName(), product.get().getPrice(), ++qty, product.get().getImage()));
+            } else {
+                cart.put(id, new Cart(id, product.get().getName(), product.get().getPrice(), 1, product.get().getImage()));
+                session.setAttribute("cart", cart);
+            }
+        }
+
+        HashMap<Integer, Cart> cart = (HashMap<Integer, Cart>) session.getAttribute("cart");
+
+        int size = 0;
+        double total = 0;
+
+        for (Cart value : cart.values()) {
+            size += value.getQuantity();
+            total += value.getQuantity() * Double.parseDouble(value.getPrice());
+        }
+
+        model.addAttribute("size", size);
+        model.addAttribute("total", total);
+
+
+        return "cart_view";
+    }
+}
